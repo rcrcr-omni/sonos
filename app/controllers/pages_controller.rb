9bf989ef6c3e9dc8401a98021975d5285a90bede
Charlie Rowat <rcrowat@gmail.com>
2015-11-30 21:47:44 +0000

    added ability to add tracks to spotify

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,45 +11,71 @@ class PagesController < ApplicationController
 
   def sonos_control
     puts 'received a command'
-    @user_id = params[:user_id]
-    @user = params[:user_name]
-    @user_object = "<@#{@user_id}|#{@user}>"
-    @text = params[:text]
+    if params[:token] == ENV['SLACK_TOKEN']
+      @user_id = params[:user_id]
+      @user = params[:user_name]
+      @user_object = "<@#{@user_id}>"
+      @text = params[:text]
 
-    options = {
-      icon_emoji: 'arrow_forward'
-    }
-    @poster = Slack::Poster.new('https://hooks.slack.com/services/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', options)
+      @poster = Slack::Poster.new(ENV['INCOMING_SLACK_TOKEN'])
 
-    if @text == 'play'
-      @speaker.play
-      @msg = @user_object + " started the sonos playing! Good job!" 
-      @poster.send_message(@msg)
+      if @text == 'play'
+        @speaker.play
+        @msg = @user_object + " started the sonos playing! Good job!" 
+        @poster.send_message(@msg)
+        render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
+
+      
+      elsif @text == 'pause'
+        @speaker.pause
+        @msg = @user_object + " just paused the Sonos"
+        @poster.send_message(@msg)
+        render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
+
+
+      elsif @text == 'skip'
+        @msg = @user_object + " just skipped *#{@speaker.now_playing[:title]}* by _#{@speaker.now_playing[:artist]}_."
+        @speaker.next
+        @poster.send_message(@msg)
+        render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
+
+      elsif @text.include?('add')
+          @search = @text.downcase.slice('add')
+          if @search.include?("by")
+            @search = @search.split("by").map(&:strip)
+            @track = @search[0]
+            @artist = @search[1]
+            @tracks = RSpotify::Track.search(@track).sort_by(&:popularity).reverse!
+            @your_track = @tracks.find {|t| t.artists[0].name.downcase == @artist}
+          else 
+            @tracks = RSpotify::Track.search(@search).sort_by(&:popularity).reverse!
+            @your_track = @tracks.first 
+          end  
+
+          # add the track to the queue
+          @speaker.add_spotify_to_queue({id: @your_track.id})
+
+          # send the message to the slack channel
+          @msg = "#{@user_object} just added *#{@your_track.name}* by *#{@your_track.artists[0].name}* to the queue: #{@your_track.external_urls['spotify']}"
+          puts @msg
+          
+          @poster.send_message(@msg)
+          render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
+
+      elsif @text == 'playing'
+        @msg = "You are listening to *#{@speaker.now_playing[:title]}* by _#{@speaker.now_playing[:artist]}_"
+        render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
+      else
+        @msg = "You need to tell me what you want to do!\nplay (play the sonos) | pause (pause the sonos) | skip (skip this track) | add [track] by [artist] (add a track to the queue - artist optional) | playing (find out what is playing now)"
+        render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
+
+      end
+
+    else # if slack token doesn't match
+      puts "Not sure how this happened, but whoever is trying to access this isn't doing it from the right slack team"
+      @msg = "I'm sorry, you're not authorised to do that!"
       render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
-
-    
-    elsif @text == 'pause'
-      @speaker.pause
-      @msg = @user_object + " just paused the Sonos"
-      @poster.send_message(@msg)
-      render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
-
-
-    elsif @text == 'skip'
-      @msg = @user_object + " just skipped *#{@speaker.now_playing[:title]}* by _#{@speaker.now_playing[:artist]}_."
-      @speaker.next
-      @poster.send_message(@msg)
-      render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
-
-
-    elsif @text == 'playing'
-      @msg = "You are listening to *#{@speaker.now_playing[:title]}* by _#{@speaker.now_playing[:artist]}_"
-      render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
-    else
-      @msg = "You need to tell me what you want to do! play (play the sonos) pause (pause the sonos) skip (skip this track) playing (find out what is playing now)"
-      render :file => "/pages/msg.json.erb", :content_type => 'application/json' and return
-
-    end
+    end # end slack token check 
 
   end
 
